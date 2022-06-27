a714cd0a44973c96c9edfcadee06e40a6be01b99
Phan Kien <phankien110302@gmail.com>
2022-06-27 13:09:07 +0700

    ExceptionHandler

---
+++ src/main/java/Demo/RestfulAPI/ExceptionHandler/NotFoundException.java
@@ -0,0 +1,10 @@
+package Demo.RestfulAPI.ExceptionHandler;
+
+public class NotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public NotFoundException(String message) {
+		super(message);
+	}
+}
---
+++ src/main/java/Demo/RestfulAPI/ExceptionHandler/ProductExceptionHandler.java
@@ -0,0 +1,30 @@
+package Demo.RestfulAPI.ExceptionHandler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+@RestControllerAdvice
+public class ProductExceptionHandler  {
+
+
+	@ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public static ErrorMessage handlerNotFoundException(NotFoundException ex, WebRequest req) {
+        // Log err
+
+        return new ErrorMessage(HttpStatus.NOT_FOUND, ex.getMessage());
+    }
+
+    // Xử lý tất cả các exception chưa được khai báo
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public static ErrorMessage handlerException(Exception ex, WebRequest req) {
+        // Log err
+
+        return new ErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+    }
+
+}
